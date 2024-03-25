3c3e44f864e46f85bd5580893098e0a01eb15656
goshva <sha.egor@gmail.com>
2024-03-25 09:49:50 +0300

    gogogo

---
+++ src/components/Article.vue
@@ -0,0 +1,69 @@
+
+  
+  <script setup>
+  import { defineProps } from 'vue';
+
+  const props = defineProps({
+  title: String,
+  message: String,
+  });
+
+  import { storeToRefs } from 'pinia';
+  
+  import { useAuthStore, useUsersStore } from '@/stores';
+  
+  const authStore = useAuthStore();
+  const { user: authUser } = storeToRefs(authStore);
+  
+  const usersStore = useUsersStore();
+  const { users } = storeToRefs(usersStore);
+  
+  usersStore.getAll();
+  
+  </script>
+  
+  <template>
+    <div class="jumbotron">
+      <div class="article-header">
+        <input model={{title}}>
+      </div>
+      <div class="article-body">  
+        <textarea model={{ message }}></textarea>
+      </div>
+    </div>
+  </template>
+
+  <style>
+  .jumbotron {
+  background-color: #f2f2f2;
+  padding: 2rem;
+  border-radius: 0.3rem;
+  }
+  /* Style for the header of the article */
+.article-header {
+  width:100%;
+  background-color: #f2f2f2;
+  padding: 20px;
+  text-align: center;
+}
+
+.article-header input {
+  width:100%;
+  font-size: 24px;
+  color: #333;
+}
+
+/* Style for the body of the article */
+.article-body {
+  margin:5px 0;
+  padding:3px;
+  display: block;
+}
+
+
+textarea {
+  width:100%;
+  padding: 20px;
+  line-height: 1.6;
+}
+  </style>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,33 @@
+<script setup>
+import Article from '@/components/Article.vue';
+
+
+import { storeToRefs } from 'pinia';
+
+import { useAuthStore, useUsersStore } from '@/stores';
+
+const authStore = useAuthStore();
+const { user: authUser } = storeToRefs(authStore);
+
+const usersStore = useUsersStore();
+const { users } = storeToRefs(usersStore);
+
+usersStore.getAll();
+</script>
+
+<template>
+    <div>
+        <h1>Hi {{authUser?.firstName}}!</h1>
+        <ul v-if="users.length">
+            <li v-for="user in users" :key="user.id">
+                <Article title="Как изучить Vue" message="с начала"></Article>
+            </li>
+        </ul>
+
+        <ul v-if="users.length">
+            <li v-for="user in users" :key="user.id">{{user.firstName}} {{user.lastName}}</li>
+        </ul>
+        <div v-if="users.loading" class="spinner-border spinner-border-sm"></div>
+        <div v-if="users.error" class="text-danger">Error loading users: {{users.error}}</div>
+    </div>
+</template>
