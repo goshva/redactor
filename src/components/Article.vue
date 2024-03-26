6bc5d9c46fac57007db56b0b242613833a499c12
МакПук <demo@MacPook-Pro.local>
2024-03-26 16:53:21 +0600

    (backend) add in Article - needs to be improved

--- src/components/Article.vue
+++ src/components/Article.vue
@@ -1,63 +1,108 @@
+<template>
+  <div class="container">
+    <h1>ТЕСТ</h1>
+    <ul>
+      <li class="one-arr" v-for="(array, index) in contentArrays" :key="index">
+        <h3>Блок-массив {{ index }}</h3>
+        <ul class="arr-list">
+          <li class="arr-item" v-for="(item, key, idx) in array" :key="idx">
+            <p class="key">{{ key }}: </p>
+            <input class="inp" type="text" :value="item" @input="updateItem(index, key, idx, $event.target.value)">
+          </li>
+        </ul>
 
-  
-  <script setup>
-  import { defineProps } from 'vue';
-  import { storeToRefs } from 'pinia';
-  import { useAuthStore, useUsersStore } from '@/stores';
-  import ButtonSave from './ButtonSave.vue';
-  
-  const props = defineProps({
-    title: String,
-    message: String,
-  });
-  
-  const authStore = useAuthStore();
-  const usersStore = useUsersStore();
-  const { users } = storeToRefs(usersStore);
-  
-  usersStore.getAll();
-  </script>
-  
-  <template>
-    <div class="jumbotron">
-      <div class="article-header">
-        <span class="span-header">Заголовок</span>
-        <textarea class="form-control" :value="title"></textarea>
-      </div>
-      <div class="article-body">
-        <span class="span-header">Текст</span>
-        <textarea class="text form-control" :value="message"></textarea>
-      </div>
-      <ButtonSave @save-changes="saveChanges"/>
-    </div>
-  </template>
+      </li>
+    </ul>
+  </div>
+</template>
 
-  <style>
-  .jumbotron {
-  background-color: #f2f2f2;
-  padding: 2rem;
-  border-radius: 0.3rem;
+<script>
+import { ref, watch } from 'vue';
+
+
+export default {
+  setup() {
+    const contentArrays = ref([]);
+
+    const fetchJsonFile = async (url) => {
+      const response = await fetch(url);
+      const jsonData = await response.json();
+      contentArrays.value = jsonData;
+    };
+
+    watch(contentArrays, () => {
+      localStorage.setItem('contentArrays', JSON.stringify(contentArrays.value));
+    });
+
+    fetchJsonFile('/public/contents.json');
+
+    if (localStorage.getItem('contentArrays')) {
+      contentArrays.value = JSON.parse(localStorage.getItem('contentArrays'));
+    }
+
+    const updateItem = (arrayIndex, key, itemIndex, value) => {
+      contentArrays.value[arrayIndex][key][itemIndex] = value;
+    };
+
+    return {
+      contentArrays,
+      updateItem
+    };
   }
-  /* Style for the header of the article */
-.article-body,
-.article-header {
-  width:100%;
-  background-color: #f2f2f2;
+};
+</script>
+
+<style scoped>
+.container {
+  max-width: 1440px;
+  background-color: #f8f9fa;
+  margin: 0 auto;
+}
+
+.one-arr {
+  margin-bottom: 20px;
+  border: 3px solid gray;
+  border-radius: 20px;
+  padding: 15px 20px;
+  background-color: #dadada;
+}
+
+.arr-list {
+  display: flex;
+  flex-wrap: wrap;
+  list-style-type: none;
+  padding: 0;
+  gap: 30px;
+}
+
+.arr-item {
+  display: flex;
+  align-items: center;
+  width: 100%;
+}
+
+.inp {
+  border: 1px solid #dadada;
+  display: block;
+  height: 52px;
+  padding: 10px 30px;
+  width: 100%;
+  height: 200px;
+  border-radius: 0 20px 20px 0;
+}
+
+.key {
+  margin: 0;
+  display: flex;
+  align-items: center;
+  justify-content: space-around;
   text-align: center;
-  max-height: 200px;
+  border: 2px solid #dadada;
+  height: 200px;
+  width: 200px;
+  text-wrap: wrap;
+  background-color: lightgreen;
+  border-radius: 20px 0 0 20px;
 }
 
-textarea {
-  width:100%;
-  font-size: 24px;
-  color: #333;
-  resize: none;
-  border-radius: 15px;
-}
-
-.article-body textarea {
-  height: 150px;
-}
-
-
-  </style>
+</style>
