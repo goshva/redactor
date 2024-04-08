00e331af4944d989b118eb86053744f3d9854297
Egor Konovalov <gavnoodaw@gmail.com>
2024-04-08 09:45:38 +0300

    Больше не появляется ошибки при добавлении меню

--- src/stores/articles.store.js
+++ src/stores/articles.store.js
@@ -1,33 +1,55 @@
 import { defineStore } from 'pinia';
-
 import { fetchWrapper } from '@/helpers';
 
-//const APIUrl = `${import.meta.env.VITE_API_URL}/api/ids`;
 const APIUrl = "https://tender.one/api/";
 
 export const useArticlesStore = defineStore({
     id: 'articles',
     state: () => ({
-        articles: [],
-        articlesList: [],
-        lastArticle: 1
+        articles: [], 
+        articlesList: [], 
+        lastArticle: 1, 
     }),
     actions: {
         async getAllList() {
-            //this.articles = { loading: true };
-            fetchWrapper.get(APIUrl)
-                .then(articlesListServer => {
-                    this.articlesList = articlesListServer.map(item => item.id);
-                })
-                .catch(error => this.articlesList = { error })
+            try {
+                const articlesListServer = await fetchWrapper.get(APIUrl);
+                const uniqueIds = [...new Set(articlesListServer.map(article => article.id))];
+                this.articlesList = uniqueIds.slice(0, 138);
+                this.loadArticles();
+            } catch (error) {
+                console.error('Ошибка при загрузке списка идентификаторов:', error);
+            }
+        },
+        async loadArticles() {
+            try {
+                this.articles = await Promise.all(
+                    this.articlesList.map(id => fetchWrapper.get(APIUrl + "?id=" + id))
+                );
+            } catch (error) {
+                console.error('Ошибка при загрузке меню:', error);
+            }
         },
         async appendNewArticle() {
-            fetchWrapper.get(APIUrl+"?id="+this.lastArticle)
-                .then(article => {
-                    this.articles.push(article)
-                })
-                .catch(error => this.article = { error })
-            this.lastArticle++
+            try {
+                const lastId = this.articlesList.length > 0 ? this.articlesList[this.articlesList.length - 1] : 0;
+                
+                if (lastId >= 138) {
+                    return;
+                }
+                
+                const article = await fetchWrapper.get(APIUrl + "?id=" + (lastId + 1));
+                
+                if (!article) {
+                    console.error('Меню с id ' + (lastId + 1) + ' не найдена.');
+                    return;
+                }
+                
+                this.articles.push(article);
+                this.articlesList.push(article.id);
+            } catch (error) {
+            }
         }
+        
     }
 });
